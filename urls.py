6f3bd90ab66c3c98dc85ed0cfc615f212c489524
AJAY <42473536+ajay0023130@users.noreply.github.com>
2021-03-25 16:14:37 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from face_detector import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('face_detector/detect/',views.detect)
-]
